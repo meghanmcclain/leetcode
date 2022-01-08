02535670c6bf368cb4875e3b340b9f7729cb62f2
Meghan McClain <61612006+meghanmcclain@users.noreply.github.com>
2022-01-07 21:28:13 -0600

    Update twoSum.py
    Added notes

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,8 @@
+"""LeetCode - Easy - TwoSum - Python
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order."""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 	    hashmap = {}
@@ -11,3 +16,11 @@ class Solution:
 	    nums = [2,7,11,15]
 	    target = 9
 	    print(self.twoSum(nums, target))
+		
+"""One-Pass Hash Table
+It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table, 
+we also look back to check if current element's complement already exists in the hash table. 
+If it exists, we have found a solution and return the indices immediately.
+
+Time complexity: O(n). We traverse the list containing n elements only once. Each lookup in the table costs only O(1) time.
+Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements."""
